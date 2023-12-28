9565bc63b6abbbea657a59ab2598174d17ba224e
Islam <islamka.1994@mail.ru>
2023-12-28 17:04:38 +0300

    fix tests/test_product.py file

--- tests/test_product.py
+++ tests/test_product.py
@@ -1,30 +1,26 @@
-import pytest
-
 from src.product import Product
 
 
-@pytest.fixture
-def product_obj():
-    return Product("iphone 11", "for calls", 59990.00, 5)
+def test_create_product():
+    product_obj1 = Product("iphone 11", "for calls", 59990.00, 5)
+    assert len(product_obj1.products) == 1
+    Product.create_product("iphone 11", "for calls", 59990.00, 5)
+    assert len(product_obj1.products) == 1
+    Product.create_product("iphone 12", "for games", 59990.00, 5)
+    assert len(product_obj1.products) == 2
+    Product.create_product("iphone 11", "for calls", 59990.00, 5)
+    assert len(product_obj1.products) == 2
 
 
-def test_init(product_obj):
+def test_init():
+    product_obj = Product("iphone 11", "for calls", 59990.00, 5)
     assert product_obj.name == "iphone 11"
     assert product_obj.description == "for calls"
     assert product_obj.quantity == 5
 
 
-def test_price(product_obj):
+def test_price():
+    product_obj = Product("iphone 11", "for calls", 59990.00, 5)
     assert product_obj.price == 59990.00
     product_obj.price = 62000.00
     assert product_obj.price == 62000.00
-
-
-def test_create_product():
-    assert len(Product.products) == 0
-    Product.create_product("iphone 11", "for calls", 59990.00, 5)
-    assert len(Product.products) == 1
-    Product.create_product("iphone 12", "for games", 59990.00, 5)
-    assert len(Product.products) == 2
-    Product.create_product("iphone 11", "for calls", 59990.00, 5)
-    assert len(Product.products) == 2
